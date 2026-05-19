39823577fbab33ac63c06623a55f45c7d38f3f05
João Pedro Paulino <joaopedrodesouzapaulino60@gmail.com>
2026-05-19 08:35:02 -0300

    Add files via upload

---
+++ aula(19.05.26)/app.py
@@ -0,0 +1,18 @@
+from flask import Flask , render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route ('/index')
+def index():
+    return render_template('index.html', usuario=None , nome=None, title='Nome')
+
+@app.route('/contato')
+def contato ():
+    nome = 'JP'
+    return render_template('index.html', title= 'Página inicial ', nome=nome , usuario=None )   
+
+@app.route('/usuario ')
+def usuario ():
+    usuario = {' nome ': 'JP ', 'email ': 'joaopedropaulino122@gmail.com'}
+    return render_template('index.html ', title = ' Página Inicial', usuario=usuario, nome=None  ) 
