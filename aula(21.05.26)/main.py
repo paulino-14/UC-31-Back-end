1e00d8a98cf8ee6ad8be845a212919da590d0ea0
João Pedro Paulino <joaopedrodesouzapaulino60@gmail.com>
2026-05-27 17:16:50 -0300

    Add files via upload

--- aula(21.05.26)/main.py
+++ aula(21.05.26)/main.py
@@ -1,18 +1,32 @@
-from flask import Flask , render_template
-from flask import request
+from flask import Flask, render_template, request
 
-app = Flask (__name__)
+app = Flask(__name__)
 
-@app.route ("/")
-@app.route ("/index")
-def index():
+@app.route('/login')
+def login():
     return render_template('login.html')
-    
-@app.route('/autenticar ', methods = {'GET'})
-def autenticar():
-     usuario = request.args.get ('usuario')
-     senha = request.args.get ('senha')
-     return"Usuário{} e senha {}". format(usuario, senha )
 
-if __name__=='__main__':
-      app.run(debug=True)
+@app.route('/recebedados', methods=['POST'])
+def recebedados():
+    nome = request.form.get('nome')
+    email = request.form.get('email')
+    return f"Nome: {nome}<br>E-mail: {email}"
+
+@app.route('/formregistrar')
+def formregistrar():
+    return render_template('registrar.html')
+
+@app.route('/registrar', methods=['POST'])
+def registrar():
+    usuario = request.form.get('usuario')
+    email = request.form.get('email')
+    senha = request.form.get('senha')
+    confirmar = request.form.get('confirmar')
+
+    if senha == confirmar:
+        return f"Usuário {usuario} registrado com sucesso!"
+    else:
+        return "As senhas não conferem."
+
+if __name__ == '__main__':
+    app.run(debug=True)
